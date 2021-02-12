1924316d19dd491af4e2b47beda1b81f083747bc
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2021-02-12 21:35:24 +0100

    Add filters feature to card list

--- app/Http/Livewire/CardList.php
+++ app/Http/Livewire/CardList.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Brand;
 use App\Models\Card;
+use App\Models\Category;
+use App\Models\Release;
 use Livewire\Component;
 
 class CardList extends Component
 {
     public $query;
+    public $filters;
     public $cards;
 
     public function mount()
     {
         $this->query = '';
+        $this->filters = [
+            'category' => '',
+            'release_name' => '',
+            'brand_name' => '',
+            'year' => ''
+        ];
         $this->cards = Card::all();
     }
 
@@ -22,8 +32,57 @@ class CardList extends Component
             ->get();
     }
 
+    public function onFilterChange()
+    {
+        $cards = Card::query();
+
+        if (!empty($this->filters['category'])) {
+            $cards = $cards->where('category', $this->filters['category']);
+        }
+        if (!empty($this->filters['release_name'])) {
+            $cards = $cards->where('release_name', $this->filters['release_name']);
+        }
+        if (!empty($this->filters['brand_name'])) {
+            $cards = $cards->where('brand_name', $this->filters['brand_name']);
+        }
+        if (!empty($this->filters['year'])) {
+            $hasEndYear = count($years = explode('-', $this->filters['year'])) === 2;
+            $startYear = $years[0];
+
+            if ($hasEndYear) {
+                $endYear = $years[1];
+                $cards = $cards->whereBetween('year', [$startYear, $endYear]);
+            } else {
+                $cards = $cards->where('year', '>=', $startYear);
+            }
+        }
+
+        $this->cards = $cards->get();
+    }
+
     public function render()
     {
-        return view('livewire.card-list');
+        return view('livewire.card-list', [
+            'categories' => Category::orderBy('name')->get(),
+            'brands' => Brand::orderBy('name')->get(),
+            'releases' => Release::orderBy('name')->get(),
+            'years' => [
+                '2020',
+                '2010-2019',
+                '2000-2009',
+                '1990-1999',
+                '1980-1989',
+                '1970-1979',
+                '1960-1969',
+                '1950-1959',
+                '1940-1949',
+                '1930-1939',
+                '1920-1929',
+                '1910-1919',
+                '1900-1909',
+                '1890-1899',
+                '1880-1889',
+            ]
+        ]);
     }
 }
--- resources/views/livewire/card-list.blade.php
+++ resources/views/livewire/card-list.blade.php
@@ -14,6 +14,43 @@
             <a href="{{ route('cards.create') }}" class="btn btn-primary">Add Card</a>
         </div>
     </div>
+    <div class="row mb-3">
+        <div class="col-12">
+            <h3>Filters</h3>
+        </div>
+        <div class="col-md-3">
+            <select wire:change="onFilterChange" wire:model="filters.category" class="custom-select text-capitalize">
+                <option value="">Category</option>
+                @foreach($categories as $category)
+                <option value="{{ $category->name }}">{{ $category->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-md-3">
+            <select wire:change="onFilterChange" wire:model="filters.brand_name" class="custom-select text-capitalize">
+                <option value="">Brand name</option>
+                @foreach($brands as $brand)
+                <option value="{{ $brand->name }}">{{ $brand->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-md-3">
+            <select wire:change="onFilterChange" wire:model="filters.release_name" class="custom-select text-capitalize">
+                <option value="">Release name</option>
+                @foreach($releases as $release)
+                <option value="{{ $release->name }}">{{ $release->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-md-3">
+            <select wire:change="onFilterChange" wire:model="filters.year" class="custom-select text-capitalize">
+                <option value="">Year</option>
+                @foreach($years as $year)
+                <option value="{{ $year }}">{{ $year }}{{ count(explode('-', $year))==1?' +':'' }}</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
     <div class="row">
         @foreach ($cards as $card)
         <div class="col-md-4 mb-3">
