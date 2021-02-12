ee75388082c8ac00667686a0b400854c897d01e8
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2021-02-12 21:11:18 +0100

    Add search by card title feature

--- app/Http/Livewire/CardList.php
+++ app/Http/Livewire/CardList.php
@@ -7,12 +7,23 @@ use Livewire\Component;
 
 class CardList extends Component
 {
+    public $query;
+    public $cards;
+
+    public function mount()
+    {
+        $this->query = '';
+        $this->cards = Card::all();
+    }
+
+    public function search()
+    {
+        $this->cards = Card::where('title', 'like', '%' . $this->query . '%')
+            ->get();
+    }
+
     public function render()
     {
-        $cards = Card::query();
-
-        return view('livewire.card-list', [
-            'cards' => $cards->get()
-        ]);
+        return view('livewire.card-list');
     }
 }
--- resources/views/livewire/card-list.blade.php
+++ resources/views/livewire/card-list.blade.php
@@ -1,6 +1,16 @@
 <div class="container">
     <div class="row mb-3">
         <div class="col">
+            <form wire:submit.prevent="search" class="row align-items-center no-gutters">
+                <div class="col mr-3">
+                    <input type="text" class="form-control" wire:model="query" placeholder="Write a card title">
+                </div>
+                <div class="col-auto">
+                    <button class="btn btn-primary px-5" type="submit" wire:click="search">Search</button>
+                </div>
+            </form>
+        </div>
+        <div class="col-auto">
             <a href="{{ route('cards.create') }}" class="btn btn-primary">Add Card</a>
         </div>
     </div>
